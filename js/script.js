f2f488c130fdb49f8a06861ca7c20ed9d4c88234
Moch Arrizal <mocharrizal12@gmail.com>
2025-03-08 21:49:54 +0700

    Desain halaman dashboard awal

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const chart = document.getElementById("myChart");
+new Chart(chart, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [1, 2, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
